e3c48a90bf27fafa2466a5dc1f561dc4f4787178
Rédoine SEREIR <redoine.sereir@42merge.com>
2025-08-31 11:49:23 +0200

    feature(api): api packages configuration

---
+++ api/src/State/AccountProcessor.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Account;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class AccountProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): Account
+    {
+        if ($data->plainPassword) {
+            $hashedPassword = $this->passwordHasher->hashPassword($data, $data->plainPassword);
+            $data->setPassword($hashedPassword);
+            $data->plainPassword = null; // Clear plain password
+        }
+
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+
+        return $data;
+    }
+}
