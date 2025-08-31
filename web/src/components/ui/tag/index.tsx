2c38c4526facfc5890dff8ed7dc28ac3a8fd2345
Rédoine SEREIR <redoine.sereir@42merge.com>
2025-08-31 18:58:18 +0200

    feature(web): add tag component to design system

---
+++ web/src/components/ui/tag/index.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+import { Tag } from "antd";
+import type { TagProps } from "antd";
+
+export default function Tag(props: TagProps) {
+  return <Tag {...props} />;
+}
